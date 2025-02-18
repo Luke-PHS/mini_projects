2053e4dc313da50923d4ae68cbd54fa7d4d09d02
Luke-PHS <144828720+Luke-PHS@users.noreply.github.com>
2025-02-18 18:51:56 +0000

    practice

--- practice1.py
+++ practice1.py
@@ -1,1 +1,25 @@
 
+
+
+# c = 9 *a
+# d = b / 3
+
+# d = int(d)
+
+# print(c)
+# print(d)
+
+# num1 = int(input("what number do you want"))
+# print(num1 * 2 )
+
+# first_name = "noah"
+# last_name = "marestaing"
+
+# def name_printer():
+#     first_name = input("first name here")
+#     last_name = input("last name here")
+
+# print(first_name + " " + last_name)
+
+# name_printer()
+
