448fffcc156b925084e2960608fe75e7d19d78ab
Luke-PHS <144828720+Luke-PHS@users.noreply.github.com>
2025-02-24 19:26:34 +0000

    help

---
+++ exercise3file.py
@@ -0,0 +1,26 @@
+groceries = ["cheez its", "nerds gummy clusters", "doritos", "cocoa puffs", "pepsi", "dr. pepper", "mountain dew"]
+while True:
+    
+a = input("Choose an item to remove or say stop to not remove anything")
+if a = "cheez its"
+    groceries.remove("cheez its")
+    print(groceries)
+elif a = "nerds gummy clusters"
+    groceries.remove("nerds gummy clusters")
+    print(groceries)
+elif a = "doritos"
+    groceries.remove("doritos")
+    print(groceries)
+elif a = "cocoa puffs"
+    groceries.remove("cocoa puffs")
+    print(groceries)
+elif a = "pepsi"
+    groceries.remove("pepsi")
+    print(groceries)
+elif a = "dr. pepper"
+    groceries.remove("dr. pepper")
+    print(groceries)
+elif a = "mountain dew"
+    groceries.remove("mountain dew")
+    print(groceries)
+ 
