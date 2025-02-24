6cd0085c5d1d017dba23d68d3a6cd30a880c9664
Luke-PHS <144828720+Luke-PHS@users.noreply.github.com>
2025-02-24 19:04:47 +0000

    done

---
+++ listp3.py
@@ -0,0 +1,40 @@
+movies = ["The Thing", "Animal House", "Rush Hour", " Rush Hour 2"]
+
+print(movies[0])
+
+for movie in movies:
+    print(str(num + "." + movie))
+    num = num + 1
+
+movies.sort()
+print(movies)
+
+movies.append("Fanatasic 4")
+print(movies) 
+
+
+movies.pop()
+movies.pop()
+print(movies)
+
+movies.remove("Animal HOuse")
+print(movies)
+
+movies.inaert(1, "Deadpool")
+print(movies)
+
+print(len(movies))
+
+print(movies[len(movies) - 1])
+
+numbers - [1, 2, 2, 9, 2, 2, 12, 9,]
+for num in numbers:
+
+while 2 in numbers:
+    number.remove(num)
+
+print(numbers)
+
+name = "noah marestaing"
+for letter in name:
+    print(letter)
