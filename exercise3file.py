4c7c4e283c117eef2d0e8f5a287aa9b26b6a52eb
Luke-PHS <144828720+Luke-PHS@users.noreply.github.com>
2025-03-03 19:05:57 +0000

    did my best

--- exercise3file.py
+++ exercise3file.py
@@ -49,127 +49,127 @@ elif b == "mountain dew":
 elif b == "stop":
     print(groceries)
 
-    c = input("Choose an item to remove again or say stop to stop")
-    if c == "cheez its":
+c = input("Choose an item to remove again or say stop to stop")
+if c == "cheez its":
         groceries.remove("cheez its")
         print(groceries)
-    elif c == "nerds gummy clusters":
+elif c == "nerds gummy clusters":
         groceries.remove("nerds gummy clusters")
         print(groceries)
-    elif c == "doritos":
+elif c == "doritos":
         groceries.remove("doritos")
         print(groceries)
-    elif c == "cocoa puffs":
+elif c == "cocoa puffs":
         groceries.remove("cocoa puffs")
         print(groceries)
-    elif c == "pepsi":
+elif c == "pepsi":
         groceries.remove("pepsi")
         print(groceries)
-    elif c == "dr. pepper":
+elif c == "dr. pepper":
         groceries.remove("dr. pepper")
         print(groceries)
-    elif c == "mountain dew":
+elif c == "mountain dew":
         groceries.remove("mountain dew")
         print(groceries)
-    elif c == "stop":
+elif c == "stop":
         print(groceries)
 
-    d = input("Choose an item to remove again or say stop to stop")
-    if d == "cheez its":
+d = input("Choose an item to remove again or say stop to stop")
+if d == "cheez its":
         groceries.remove("cheez its")
         print(groceries)
-    elif d == "nerds gummy clusters":
+elif d == "nerds gummy clusters":
         groceries.remove("nerds gummy clusters")
         print(groceries)
-    elif d == "doritos":
+elif d == "doritos":
         groceries.remove("doritos")
         print(groceries)
-    elif d == "cocoa puffs":
+elif d == "cocoa puffs":
         groceries.remove("cocoa puffs")
         print(groceries)
-    elif d == "pepsi":
+elif d == "pepsi":
         groceries.remove("pepsi")
         print(groceries)
-    elif d == "dr. pepper":
+elif d == "dr. pepper":
         groceries.remove("dr. pepper")
         print(groceries)
-    elif d == "mountain dew":
+elif d == "mountain dew":
         groceries.remove("mountain dew")
         print(groceries)
-    elif d == "stop":
+elif d == "stop":
         print(groceries)
 
-    e = input("Choose an item to remove again or say stop to stop")
-    if e == "cheez its":
+e = input("Choose an item to remove again or say stop to stop")
+if e == "cheez its":
         groceries.remove("cheez its")
         print(groceries)
-    elif e == "nerds gummy clusters":
+elif e == "nerds gummy clusters":
         groceries.remove("nerds gummy clusters")
         print(groceries)
-    elif e == "doritos":
+elif e == "doritos":
         groceries.remove("doritos")
         print(groceries)
-    elif e == "cocoa puffs":
+elif e == "cocoa puffs":
         groceries.remove("cocoa puffs")
         print(groceries)
-    elif e == "pepsi":
+elif e == "pepsi":
         groceries.remove("pepsi")
         print(groceries)
-    elif e == "dr. pepper":
+elif e == "dr. pepper":
         groceries.remove("dr. pepper")
         print(groceries)
-    elif e == "mountain dew":
+elif e == "mountain dew":
         groceries.remove("mountain dew")
         print(groceries)
-    elif e == "stop":
+elif e == "stop":
         print(groceries)
 
-    f = input("Choose an item to remove again or say stop to stop")
-    if f == "cheez its":
+f = input("Choose an item to remove again or say stop to stop")
+if f == "cheez its":
         groceries.remove("cheez its")
         print(groceries)
-    elif f == "nerds gummy clusters":
+elif f == "nerds gummy clusters":
         groceries.remove("nerds gummy clusters")
         print(groceries)
-    elif f == "doritos":
+elif f == "doritos":
         groceries.remove("doritos")
         print(groceries)
-    elif f == "cocoa puffs":
+elif f == "cocoa puffs":
         groceries.remove("cocoa puffs")
         print(groceries)
-    elif f == "pepsi":
+elif f == "pepsi":
         groceries.remove("pepsi")
         print(groceries)
-    elif f == "dr. pepper":
+elif f == "dr. pepper":
         groceries.remove("dr. pepper")
         print(groceries)
-    elif f == "mountain dew":
+elif f == "mountain dew":
         groceries.remove("mountain dew")
         print(groceries)
-    elif f == "stop":
+elif f == "stop":
         print(groceries)
 
-    g = input("Choose an item to remove again or say stop to stop")
-    if g == "cheez its":
+g = input("Choose an item to remove again or say stop to stop")
+if g == "cheez its":
         groceries.remove("cheez its")
         print(groceries)
-    elif g == "nerds gummy clusters":
+elif g == "nerds gummy clusters":
         groceries.remove("nerds gummy clusters")
         print(groceries)
-    elif g == "doritos":
+elif g == "doritos":
         groceries.remove("doritos")
         print(groceries)
-    elif g == "cocoa puffs":
+elif g == "cocoa puffs":
         groceries.remove("cocoa puffs")
         print(groceries)
-    elif g == "pepsi":
+elif g == "pepsi":
         groceries.remove("pepsi")
         print(groceries)
-    elif g == "dr. pepper":
+elif g == "dr. pepper":
         groceries.remove("dr. pepper")
         print(groceries)
-    elif g == "mountain dew":
+elif g == "mountain dew":
         groceries.remove("mountain dew")
         print(groceries)
-    elif g == "stop":
+elif g == "stop":
         print(groceries) 
